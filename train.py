4a960048e58134754e80cb2d898c8456caeb002f
Riham Chbouki <chbouki.riham@gmail.com>
2025-07-28 16:27:59 +0200

    train: lets start the implementation
    Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

---
+++ train.py
@@ -0,0 +1,12 @@
+# Initialize weights matrices and biases vectors randomly
+# calculate z the weighted sum of weights and biases
+# activation function a(z) using sigmoid or relu
+# calculate the CHAIN RULE which is the sensitivity of C (cost) to the weights
+# Define cost function (f(w, b) = x)
+# Gradient Descent to minimize cost function -> weights
+
+if __name__ == "__main__":
+    try:
+        pass
+    except Exception as e:
+        print(f"Error: {e}")
