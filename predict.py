7fb9717fe51763255c0377df70f3fc203837f655
Riham Chbouki <chbouki.riham@gmail.com>
2025-08-12 16:33:45 +0200

    predict: touch up
    Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from MLP import MLP
 import pandas as pd
 import json
@@ -15,33 +15,7 @@ def load_model(filepath):
     biases = [np.array(b) for b in model_data["biases"]]
     return weights, biases
 
-def binary_cross_entropy(y_true, y_pred):
-    eps = 1e-15
-    y_pred = np.clip(y_pred, eps, 1 - eps)
-
-    bce = - (y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
-    return np.mean(bce)
-
-
-def predict(model, X):
-    output = model.feedforward(X)  # output shape (num_samples, 1)
-    preds = (output >= 0.5).astype(int)  # threshold at 0.5: 1 = malignant, 0 = benign
-    return preds
-
-if __name__ == "__main__":
-    # Load weights and biases
-    weights, biases = load_model('cancer_detection.json')
-
-    # Create model instance with input size based on first layer weights shape
-    model = MLP(input_size=weights[0].shape[0])
-
-    # Assign loaded weights and biases to the model
-    model.weights = list(weights)
-    model.biases = list(biases)
-    model.activation_funcs = ['relu', 'relu', 'sigmoid']
-
-    print("Model loaded successfully")
-
+def read_and_scale_data():
     # Load and preprocess the dataset
     df = pd.read_csv(DATA_FILE, names=COLUMN_NAMES, header=0)
     dataset = preprocess_dataset(df)
@@ -52,19 +26,43 @@ if __name__ == "__main__":
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X_predict)
 
-    # Get raw predicted probabilities
-    probs = model.feedforward(X_scaled)
+    return X_scaled, y_true
 
-    # Calculate and print binary cross-entropy loss
-    print("y_true shape:", y_true.shape)
-    print("probs shape:", probs.shape)
-    print(probs)
+def binary_cross_entropy(y_true, y_pred):
+    eps = 1e-15
+    y_pred = np.clip(y_pred, eps, 1 - eps)
 
-    loss = binary_cross_entropy(y_true.reshape(-1, 1), probs)
-    print(f"Binary Cross-Entropy Loss: {loss:.4f}")
+    bce = - (y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    return np.mean(bce)
 
-    # predictions = predict(model, X_predict)
-    #
-    # for i, pred in enumerate(predictions):
-    #     label = "Malignant" if pred == 1 else "Benign"
-    #     print(f"Sample {i}: {label}")
+if __name__ == "__main__":
+    try:
+        # Load weights and biases
+        weights, biases = load_model('cancer_detection.json')
+
+        # Create model instance with input size based on first layer weights shape
+        model = MLP(input_size=weights[0].shape[0])
+
+        # Assign loaded weights and biases to the model
+        model.weights = weights
+        model.biases = biases
+        model.activation_funcs = ['relu', 'relu', 'sigmoid']
+
+        print("Model loaded successfully !")
+
+        X_scaled, y_true = read_and_scale_data()
+        probs = model.feedforward(X_scaled)
+        y_pred = (probs >= 0.5).astype(int)
+
+
+        # Compute metrics
+        loss = binary_cross_entropy(y_true.reshape(-1, 1), probs)
+        acc = accuracy_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+
+        print("\n--------------Prediction Metrics:--------------")
+        print(f"Binary Cross-Entropy Loss: {loss:.4f}")
+        print(f"Accuracy:  {acc:.4f}")
+        print(f"Precision: {precision:.4f}")
+    except Exception as e:
+        print(f"Error: {e}")
