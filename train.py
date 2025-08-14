7d94a9f03c6dd2e9f0c3034e9ca9896b28e2eb84
Riham Chbouki <chbouki.riham@gmail.com>
2025-08-14 14:56:26 +0200

    train: fixed argparse
    Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

--- train.py
+++ train.py
@@ -3,43 +3,40 @@ from MLP import MLP
 import argparse
 from separate import preprocess_and_split_data
 
-# def parse_args():
-#     parser = argparse.ArgumentParser(
-#         description="Train a Multi-Layer Perceptron model."
-#     )
-#
-#     parser.add_argument(
-#         "--l",
-#         '--add_layers',
-#         type=str,
-#         nargs="+",
-#         default='24 24',
-#         help="Layers' sizes to add."
-#     )
-#     parser.add_argument(
-#         '-a',
-#         '--activations',
-#         type=str,
-#         default=0.001,
-#         help="Learning rate for training."
-#     )
-#     parser.add_argument(
-#         '-lr',
-#         '--learning_rate',
-#         type=int,
-#         default=0.001,
-#         help="Learning rate for training."
-#     )
-#     parser.add_argument(
-#         'e',
-#         '--epochs',
-#         type=int,
-#         default=100,
-#         help="Number of epochs to train."
-#     )
+def parse_parameters():
+    parser = argparse.ArgumentParser(
+        description="Train a Multi-Layer Perceptron model for binary classification of breast cancer."
+    )
+
+    parser.add_argument("-l", '--add_layers', type=int, nargs="+", required=True, default=[24, 24], help="Layers' sizes to add.")
+    parser.add_argument('-a', '--activations', type=str, nargs="+", help="Learning rate for training.")
+    parser.add_argument('-lr', '--learning_rate', type=float, required=True, default=0.001, help="Learning rate for training.")
+    parser.add_argument('-e', '--epochs', type=int, required=True, default=100, help="Number of epochs to train.")
+
+    return parser.parse_args()
+
+def get_parameters(params):
+    layers = params.add_layers
+    activations = [act.lower() for act in params.activations]
+    learning_rate = params.learning_rate
+    epochs = params.epochs
+
+    if len(activations) != len(layers):
+        raise ValueError("Error: activation functions mismatch with number of layers.")
+
+    for act in activations:
+        if act not in {'relu', 'tanh', 'sigmoid'}:
+            raise ValueError("Error: activation function must be 'relu', 'tanh', or 'sigmoid'.")
+
+    return layers, activations, learning_rate, epochs
+
 
 if __name__ == "__main__":
     try:
+        # Get parameters from the command line
+        params = parse_parameters()
+        layers, activations, learning_rate, epochs = get_parameters(params)
+
         # Retrieve training and validation sets
         X_train, X_val, y_train, y_val = preprocess_and_split_data()
 
@@ -54,12 +51,11 @@ if __name__ == "__main__":
         # Initialize the model
         model = MLP(input_size)
 
-        # Add 3 layers
-        model.add_layer(24, activation='relu')
-        model.add_layer(24, activation='relu')
+        for layer, activation in zip(layers, activations):
+            model.add_layer(layer, activation=activation)
 
         # Train the model
-        model.fit(X_train, y_train, X_val, y_val, epochs=600, learning_rate=0.05)
+        model.fit(X_train, y_train, X_val, y_val, epochs=epochs, learning_rate=learning_rate)
 
         # Save trained MLP model
         model.save_model("cancer_detection.json")
