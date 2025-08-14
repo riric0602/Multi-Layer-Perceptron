correct new softmax/old sigmoid logic mismatches
Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

MLP.py
@@ -117,11 +117,12 @@ class MLP:
 
             # Training metrics
             output_train = self.feedforward(X_train)
-            train_loss = np.mean((output_train - y_train) ** 2)
+            train_loss = -np.mean(np.sum(y_train * np.log(output_train + 1e-15), axis=1))
             train_losses.append(train_loss)
 
-            train_preds = (output_train >= 0.5).astype(int)
-            train_accuracy = np.mean(train_preds == y_train)
+            train_preds = np.argmax(output_train, axis=1)
+            y_true = np.argmax(y_train, axis=1)
+            train_accuracy = np.mean(train_preds == y_true)
             train_accuracies.append(train_accuracy)
 
             if X_val is not None and y_val is not None:
@@ -138,7 +139,8 @@ class MLP:
     def save_model(self, filepath):
         model_data = {
             "weights": [w.tolist() for w in self.weights],
-            "biases": [b.tolist() for b in self.biases]
+            "biases": [b.tolist() for b in self.biases],
+            "activations": [a for a in self.activation_funcs]
         }
 
         with open(filepath, "w") as f:

predict.py
@@ -13,7 +13,9 @@ def load_model(filepath):
 
     weights = [np.array(w) for w in model_data["weights"]]
     biases = [np.array(b) for b in model_data["biases"]]
-    return weights, biases
+    activations = [a for a in model_data["activations"]]
+
+    return weights, biases, activations
 
 def read_and_scale_data():
     # Load and preprocess the dataset
@@ -38,7 +40,7 @@ def binary_cross_entropy(y_true, y_pred):
 if __name__ == "__main__":
     try:
         # Load weights and biases
-        weights, biases = load_model('cancer_detection.json')
+        weights, biases, activations = load_model('cancer_detection.json')
 
         # Create model instance with input size based on first layer weights shape
         model = MLP(input_size=weights[0].shape[0])
@@ -46,14 +48,13 @@ if __name__ == "__main__":
         # Assign loaded weights and biases to the model
         model.weights = weights
         model.biases = biases
-        model.activation_funcs = ['relu', 'relu', 'sigmoid']
+        model.activation_funcs = activations
 
         print("Model loaded successfully !")
 
         X_scaled, y_true = read_and_scale_data()
         probs = model.feedforward(X_scaled)
-        y_pred = (probs >= 0.5).astype(int)
-
+        y_pred = np.argmax(probs, axis=1)
 
         # Compute metrics
         loss = binary_cross_entropy(y_true.reshape(-1, 1), probs)